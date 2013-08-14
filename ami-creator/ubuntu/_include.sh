d0dc12208ebae9021737793e8d06b0cb8b0ad02d
Peter Sankauskas <pas256@gmail.com>
2013-08-13 21:59:08 -0700

    Updating company name

--- ami-creator/ubuntu/_include.sh
+++ ami-creator/ubuntu/_include.sh
@@ -1,4 +1,4 @@
-# Copyright 2013 AWS Answers LLC
+# Copyright 2013 Answers for AWS LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
