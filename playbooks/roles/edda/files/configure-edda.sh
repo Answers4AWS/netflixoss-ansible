61afd9ec9360f1c0ad945d02d961ec7ed22ac3e6
Peter Sankauskas <pas256@gmail.com>
2013-12-20 17:39:34 -0800

    A new way of choosing the region for edda

--- playbooks/roles/edda/files/configure-edda.sh
+++ playbooks/roles/edda/files/configure-edda.sh
@@ -3,5 +3,5 @@
 region=`ec2metadata --availability-zone | sed 's/.$//'`
 
 echo "Setting Edda region to $region"
-perl -i -pe "s/us-east-1/$region/" /var/lib/tomcat7/webapps/edda/WEB-INF/classes/edda.properties
+perl -i -pe "s/^edda\.region\=.*/edda.region=$region/" /var/lib/tomcat7/webapps/edda/WEB-INF/classes/edda.properties
 
